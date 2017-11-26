248c20247cc3a49ffb9fa7883d46893a977784fd
Luis Thomas <thomasluis555@gmail.com>
2017-11-26 16:49:56 -0500

    reviews can now be deleted

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :reviews
    root :to => 'pictures#index'
-  resources :pictures
+  resources :pictures do
+     resources :reviews
+  end
   resources :reviewssp
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
